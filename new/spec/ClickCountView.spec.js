df57abf9cec28aa89fa8d2a17407fceee76c1e88
kaye <kaye.lee@luxrobo.com>
2022-02-10 12:14:04 +0900

    feat: click-count-view-spec1

--- new/spec/ClickCountView.spec.js
+++ new/spec/ClickCountView.spec.js
@@ -9,6 +9,19 @@ describe('App.ClickCountView', () => {
     view = App.ClickCountView(clickCounter, updateElement)
   })
 
+  describe('의존성 모듈 검증', () => {
+    it('clickCounter를 주입하지 않으면 에러를 던진다.', () => {
+      const clickCounter = null
+      const actual = () => App.ClickCountView(clickCounter, updateElement)
+      expect(actual).toThrowError()
+    })
+    it('updateElement를 주입하지 않으면 에러를 던진다.', () => {
+      const updateElement = null
+      const actual = () => App.ClickCountView(clickCounter, updateElement)
+      expect(actual).toThrowError()
+    })
+  })
+
   describe('updateView()', () => {
     it('ClickCounter의 counter 값을 출력한다.', () => {
       view.updateView()
--- new/src/ClickCountView.js
+++ new/src/ClickCountView.js
@@ -1,6 +1,9 @@
 var App = App || {}
 
 App.ClickCountView = (clickCounter, updateElement) => {
+  if (!clickCounter) throw Error('invalid clickCounter module')
+  if (!updateElement) throw Error('invalid updateElement module')
+
   return {
     updateView() {
       updateElement.innerHTML = clickCounter.getValue()
